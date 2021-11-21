a148831d0ce3abf0746e74abd313c3adcd62f043
python290821 <90146835+python290821@users.noreply.github.com>
2021-11-21 18:51:29 +0200

    Update program.py

--- program.py
+++ program.py
@@ -5,6 +5,10 @@ select id, name from COMPANY
   where id > 2;
 select * from COMPANY where name='Paul';
 select * from COMPANY where address like 'T%';
-select * from COMPANY where address like 'T%' and SALARY > 80000
+select * from COMPANY where address like '%T%' and SALARY > 80000
 SELECT * from COMPANY where age=25 or age=30;
+select count(*) from COMPANY;
+select avg(salary) from COMPANY
+update COMPANY set age=21 where id=5;
+delete from COMPANY where address like 'Rich-Mond%';
 '''
